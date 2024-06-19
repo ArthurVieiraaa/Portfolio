dfcdd438db525a1e66bf1e207bbe423f0efa08b2
Arthur Vieira Bruske <avieirabruske@gmail.com>
2024-06-19 17:58:39 -0300

    Criando Sobre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 250)
+    header.classList.toggle('rolagem', window.scrollY > 30)
 })
 
 const text = document.querySelector(".sec-text");
@@ -19,3 +19,20 @@ const textLoad = () => {
 
 textLoad();
 setInterval(textLoad, 12000);
+
+const tabs = document.querySelectorAll('.tab-btn');
+const all_content = document.querySelectorAll('.tab-content');
+
+tabs.forEach((tab, index) =>{
+    tab.addEventListener('click', (e)=>{
+        tabs.forEach(tab => tab.classList.remove('active'));
+        tab.classList.add('active');
+
+        var line = document.querySelector('.line');
+        line.style.width = e.target.offsetWidth + "px";
+        line.style.left = e.target.offsetLeft + "px";
+
+        all_content.forEach(content => content.classList.remove('active'));
+        all_content[index].classList.add('active');
+    })
+})
